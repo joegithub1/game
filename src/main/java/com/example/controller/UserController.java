3bf9e32eef730478050d62fddbd59dedbb497088
huangjian <day_1111@126.com>
2023-05-27 18:22:47 +0800

    增加hello方法

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -12,4 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
 }
