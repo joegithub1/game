f9bee493670f1ddcc3c2f9de3975d38b578231db
huangjian <day_1111@126.com>
2023-05-27 18:09:01 +0800

    修改BUG

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @ClassName UserController
  * @Description:
  * @Author huangjian
  * @Date 2023/5/27
  **/
+@RestController
+@RequestMapping("/user")
 public class UserController {
 }
